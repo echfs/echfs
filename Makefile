818b9f90406598142cafa2e076e19f275080b043
Jakob Kirsch <jakob.kirsch@web.de>
2020-08-24 16:41:20 +0200

    Add mkfs.echfs

--- Makefile
+++ Makefile
@@ -4,13 +4,16 @@ CFLAGS=-O3 -Wall -Wextra -pipe
 
 .PHONY: all clean install
 
-all: echfs-utils echfs-fuse 
+all: echfs-utils echfs-fuse mkfs.echfs
 
 echfs-utils: echfs-utils.c part.c part.h
 	$(CC) $(CFLAGS) part.c echfs-utils.c -o echfs-utils
 
 echfs-fuse: echfs-fuse.c part.c part.h
 	$(CC) $(CFLAGS) part.c echfs-fuse.c $(shell pkg-config fuse --cflags --libs) -o echfs-fuse
+
+mkfs.echfs: mkfs.echfs.c
+	$(CC) $(CFLAGS) mkfs.echfs.c -o mkfs.echfs
 
 clean:
 	rm -f echfs-utils
---
+++ mkfs.echfs.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define RESERVED_BLOCKS         16
+
+static FILE * image;
+
+static inline void wr_qword(uint64_t loc, uint64_t x) {
+    fseek(image, (long)loc, SEEK_SET);
+    fwrite(&x, 8, 1, image);
+    return;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 4) {
+          fprintf(stderr, "%s: usage: %s <image> <bytes per block> <reserved blocks factor>\n", argv[0], argv[0]);
+          return 1;
+    }
+    image = fopen(argv[1], "rb");
+    if (image == NULL) {
+         fprintf(stderr, "%s: error: no valid image specified.\n", argv[0]);
+    }
+
+    fseek(image, 0L, SEEK_END);
+    uint64_t imgsize = (uint64_t)ftell(image);
+    rewind(image);
+    fclose(image);
+    image = fopen(argv[1], "wb");
+
+    fprintf(stderr, "%s: info: formatting %lu bytes...\n", argv[0], imgsize);
+
+    uint64_t bytesperblock = atoi(argv[2]);
+
+    if ((bytesperblock <= 0) || (bytesperblock % 512)) {
+        fprintf(stderr, "%s: error: block size MUST be a multiple of 512.\n", argv[0]);
+        fclose(image);
+        return 1;
+    }
+
+    if (imgsize % bytesperblock) {
+        fprintf(stderr, "%s: error: image is not block-aligned.\n", argv[0]);
+        fclose(image);
+        return 1;
+    }
+
+    uint64_t reserved_factor = atoi(argv[3]);
+
+    if ((reserved_factor <= 0) || (reserved_factor >= 100)) {
+        fprintf(stderr, "%s: error: reserved blocks factor must be between 1%% and 99%%\n", argv[0]);
+        fclose(image);
+        return 1;
+    }
+
+    uint64_t blocks = imgsize / bytesperblock;
+
+    fseek(image, 4, SEEK_SET);
+    fputs("_ECH_FS_", image);
+    wr_qword(12, blocks);	// blocks
+    wr_qword(20, blocks / (100 / reserved_factor)); 	//reserved blocks
+    wr_qword(28, bytesperblock);	// block size
+    // mark reserved blocks
+    uint64_t loc = RESERVED_BLOCKS * bytesperblock;
+
+    uint64_t fatsize = (blocks * sizeof(uint64_t)) / bytesperblock;
+    uint64_t dirsize = blocks / (100 / reserved_factor);
+
+    for (uint64_t i = 0; i < (RESERVED_BLOCKS + fatsize + dirsize); i++) {
+        wr_qword(loc, 0xfffffffffffffff0);
+        loc += sizeof(uint64_t);
+    }
+    fflush(image);
+    fclose(image);
+    return 0;
+}
